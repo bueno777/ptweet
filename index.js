21e76ede3349f74352357d2bed778527919e9d62
bueno777 <bueno777.jlba@gmail.com>
2021-11-15 19:00:05 -0700

    Update index.js
    Avance de pagina web ptweet

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 window.onload=()=>{
     let usuario= JSON.parse(localStorage.getItem("usuario"));
     console.log(usuario);
-    document.getElementById("usuario").innerHTML="Hola"+usuario.nombre;
+    //document.getElementById("usuario").innerHTML="Hola"+usuario.nombre;
+    let txtTweet=document.getElementById("txtTweet");
+    let btnTweet=document.getElementById("btnTweet");
+    btnTweet.addEventListener('click',(evt)=>{
+        if(txtTweet.ariaValueMax.trim()==""){
+            let obj = {
+                img: usuario.img,
+                nombre: usuario.nombre,
+                mensaje: txtTweet.value,
+                username: usuario.username,
+            };
+            crearPost()
+        }
+
+    });
+    txtTweet.addEventListener('keyup',(evt)=>{
+
+    });
+    function crearPost(datos){
+        console.log(datos)
+
+    }
 
 };
